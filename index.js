8da63d83e6a0797f9fd2acac71d1ca32b1a8fb0c
xandegrawe <xandegrawe@gmail.com>
2022-04-05 13:52:33 -0300

    Adicionando Javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+function test(){
+    const name = document.getElementById('name').value
+    const mail = document.getElementById('mail').value
+    const area = document.getElementById('area').value
+
+    return alert('WoooooW \n\n Seja bem vindo ${name} ao bootcamp da àrea de ${area}. \n\n Você se cadastrou com o email \n ${mail}.')
+}
